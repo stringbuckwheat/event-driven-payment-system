c6925bbd9b1597cc80fa811ffc9b3c61d25047af
stringbuckwheat <stringbuckwheat@gmail.com>
2026-02-02 10:50:27 +0900

    프로젝트 세팅

---
+++ src/main/java/com/example/edps/EdpsApplication.java
@@ -0,0 +1,13 @@
+package com.example.edps;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EdpsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EdpsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/edps/EdpsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.edps;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EdpsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
